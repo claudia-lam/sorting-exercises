1e7ec75363c154ef564db9228d8ea5ee5df1079d
Claudia Lam <claudiaslam@gmail.com>
2023-06-02 11:54:47 -0700

    add solution to bubble sort

--- bubble.js
+++ bubble.js
@@ -1,3 +1,19 @@
-function bubbleSort() {}
+function bubbleSort(array) {
+
+  for(let i = array.length - 1; i >= 0; i--) {
+    console.log("array BEFORE:", array);
+    console.log("i:", array[i]);
+    for (let j = 0; j <= i - 1; j++) {
+      console.log("j:", array[j], "j+1:", array[j+1]);
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j+1]] = [array[j + 1], array[j]];
+      }
+      console.log("array AFTER SWAP:", array);
+    }
+  }
+
+  return array;
+
+}
 
 module.exports = bubbleSort;
